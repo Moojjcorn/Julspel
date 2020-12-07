39067c46020a39c14b65d0d9bc15e7569006542e
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2020-12-07 14:08:12 +0100

    Update Julspel.py

--- Julspel.py
+++ Julspel.py
@@ -11,5 +11,19 @@ while True:
         Health = 100
         Santa = 100
         print(f"\n\tYour Health {Health} ❤\n\tSantaclaus's Health {Santa} ❤")
+        while Health >= 0 and Santa >= 0:
+            YourAbility1DMG = random.randint(4,6)
+            YourAbility2DMG = random.randint(8,13)
+            YourMove = 1
+            while YourMove == 1:
+                print('\n\t[1] Ability 1: Snowball\n\t[2] Ability 2: Icicle\n')
+                YourAbility = int(input('Choose Ability: '))
+                if YourAbility == 1:
+                    Santa = Santa - YourAbility1DMG
+                    print(f'\n\tYou did {YourAbility1DMG} damage to Santa with your Snowball\n\tYou have {Health} ❤\n\tSanta has {Santa} ❤')
+                elif YourAbility == 2:
+                    Santa = Santa - YourAbility2DMG
+                    print(f'\n\tYou did {YourAbility2DMG} damage to Santa with your icicle\n\tYou have {Health} ❤\n\tSanta has {Santa} ❤')
+            
     else:
         print('Invalid input, try again')
