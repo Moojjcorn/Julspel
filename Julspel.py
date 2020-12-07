2dd8e269a4a4d6c655caabe24f4f7b8c3a82492c
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2020-12-07 15:13:29 +0100

    Update Julspel.py

--- Julspel.py
+++ Julspel.py
@@ -14,17 +14,27 @@ while True:
         while Health >= 0 and Santa >= 0:
             YourAbility1DMG = random.randint(4,6)
             YourAbility2DMG = random.randint(8,13)
-            YourMove = 1
-            while YourMove == 1:
-                print('\n\t[1] Ability 1: Snowball\n\t[2] Ability 2: Icicle\n')
-                YourAbility = int(input('Choose Ability: '))
-                if YourAbility == 1:
-                    Santa = Santa - YourAbility1DMG
-                    print(f'\n\tYou did {YourAbility1DMG} damage to Santa with your Snowball\n\tYou have {Health} ❤\n\tSanta has {Santa} ❤')
-                elif YourAbility == 2:
-                    Santa = Santa - YourAbility2DMG
-                    print(f'\n\tYou did {YourAbility2DMG} damage to Santa with your icicle\n\tYou have {Health} ❤\n\tSanta has {Santa} ❤')
-                YourMove - 1
-            print('Santas move')            
+            print('\n\t[1] Ability 1: Snowball\n\t[2] Ability 2: Icicle\n')
+            YourAbility = int(input('Choose Ability: '))
+            if YourAbility == 1:
+                Santa = Santa - YourAbility1DMG
+                print(f'\n\tYou did {YourAbility1DMG} damage to Santa with your Snowball\n\tYou have {Health} ❤\n\tSanta has {Santa} ❤')
+            elif YourAbility == 2:
+                Santa = Santa - YourAbility2DMG
+                print(f'\n\tYou did {YourAbility2DMG} damage to Santa with your Icicle\n\tYou have {Health} ❤\n\tSanta has {Santa} ❤')
+            SantaAbility1DMG = random.randint(4,6)
+            SantaAbility2DMG = random.randint(8,13)
+            SantaAbility = random.randint(1,2)
+            print('------------------------------------------------------------')
+            if SantaAbility == 1:
+                Health = Health - SantaAbility1DMG
+                print(f'\n\tSanta did {SantaAbility1DMG} damage to YOU with his Coal for Christmas\n\tYou have {Health} ❤\n\tSanta has {Santa} ❤')
+            elif SantaAbility == 2:
+                Health = Health - SantaAbility2DMG
+                print(f'\n\tSanta did {SantaAbility2DMG} damage to YOU with his flamethrower\n\tYou have {Health} ❤\n\tSanta has {Santa} ❤')
+        if Health <= 0:
+            print(f'\n\tYou have been slain by Santaclaus!\n\tDefeat')
+        elif Santa <= 0:
+            print(f'\n\tYou have slain Santaclaus!\n\tVictory!')
     else:
         print('Invalid input, try again')
